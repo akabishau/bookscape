db72fb7c780a0d004a185e4502b6985a2ec102f5
akabishau <aleksey.kabishau@icloud.com>
2024-02-05 20:16:51 -0500

    fetch user's books and display them by statuses on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@ class HomeController < ApplicationController
   def index
     @quote = "\"If you don’t like to read, you haven’t found the right book.\""
     @author = "– J.K. Rowling"
+
+    # fetch all books grouped by status avoing N+1 queries
+    @books_by_status = current_user.user_books.includes(:book).group_by(&:status)
+    @books_by_status.each do |status, user_books|
+      @books_by_status[status] = user_books.map(&:book)
+    end
   end
 end
