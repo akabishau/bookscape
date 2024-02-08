8d5db0e151bd527ef5f8991f6ec6c8fe67b97eac
akabishau <aleksey.kabishau@icloud.com>
2024-02-08 12:22:31 -0500

    move user authentication from home to application controller; render header in the application layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
   def index
     @quote = "\"If you don’t like to read, you haven’t found the right book.\""
     @author = "– J.K. Rowling"
