40fecfc3020b8dad03213bb889302a9a26b06093
akabishau <aleksey.kabishau@icloud.com>
2024-07-29 21:32:32 -0400

    add starting review model and link it to the user book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   has_many :user_books, dependent: :restrict_with_exception
+  has_many :reviews, through: :user_books
 
   validates :title, :google_id, presence: true
   validates :google_id, uniqueness: true
