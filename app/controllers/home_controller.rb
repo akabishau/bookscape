13c1507893d82971a1427171fffe69bb3f9e6535
akabishau <aleksey.kabishau@icloud.com>
2024-09-19 21:22:10 -0400

    fix rendering home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,12 @@ class HomeController < ApplicationController
     @quote = "\"If you don’t like to read, you haven’t found the right book.\""
     @author = "– J.K. Rowling"
 
-    books = UserBookService.group_user_books_by_status(current_user)
-    Rails.logger.info("Books by status for HOME: #{@books}")
-    @books_by_status = books
+    # books = UserBookService.group_user_books_by_status(current_user)
+    # Rails.logger.info("Books by status for HOME: #{@books}")
+    # @books_by_status = books
+
+    @books_by_status = current_user.user_books.includes(:book).group_by(&:status)
   end
 end
+
+# TODO: review the service implementation and use standard Rails methods
