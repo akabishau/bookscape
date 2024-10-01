b5b17d0d2fa963cf4483a0198d71be15daa57923
akabishau <aleksey.kabishau@icloud.com>
2024-10-01 09:44:54 -0400

    open dropdown menu using stimulus controller

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,30 +1,44 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['icon', 'menu', 'logo'];
+  static targets = ['icon', 'dropdown', 'logo'];
 
   connect() {
     console.log('Menu controller connected!');
   }
 
   toggle() {
-    this.element.classList.toggle('header--menu-open');
+    // Toggle the menu open/close class
+    this.element.classList.toggle('header--menu--open');
 
+    // Toggle the dropdown's visibility with a separate class
+    this._toggleDropdown();
+
+    // Toggle menu icon
+    this._toggleIcon();
+
+    // Toggle logo (black/white)
+    this._toggleLogo();
+  }
+
+  _toggleDropdown() {
+    this.dropdownTarget.classList.toggle('header__dropdown--open');
+  }
+
+  _toggleIcon() {
     const currentIcon = this.iconTarget;
-    // TODO: Refactor this...
     if (currentIcon.src.includes('menu_icon')) {
-      currentIcon.src = '/assets/close_icon.svg'; //
+      currentIcon.src = '/assets/close_icon.svg'; // Show close icon
     } else {
-      currentIcon.src = '/assets/menu_icon.svg'; //
+      currentIcon.src = '/assets/menu_icon.svg'; // Show menu icon
     }
+  }
 
-    // Check if the menu is open and switch the logo accordingly
-    if (this.element.classList.contains('header--menu-open')) {
-      this.logoTarget.src = '/assets/logo_black.svg'; // Change to black logo
+  _toggleLogo() {
+    if (this.element.classList.contains('header--menu--open')) {
+      this.logoTarget.src = '/assets/logo_black.svg'; // Switch to black logo
     } else {
-      this.logoTarget.src = '/assets/logo.svg'; // Change back to default logo
+      this.logoTarget.src = '/assets/logo.svg'; // Switch back to default logo
     }
-
-    console.log('Menu button toggled!');
   }
 }
