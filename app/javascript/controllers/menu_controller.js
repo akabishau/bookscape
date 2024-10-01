ee411296290ea01ffa3ce29c07a34c5ef5423002
akabishau <aleksey.kabishau@icloud.com>
2024-10-01 11:48:34 -0400

    add profile dropdown menu when clicked

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['icon', 'dropdown', 'logo'];
+  static targets = ['icon', 'dropdown', 'logo', 'profileDropdown'];
 
   connect() {
     console.log('Menu controller connected!');
@@ -9,7 +9,7 @@ export default class extends Controller {
 
   toggle() {
     // Toggle the menu open/close class
-    this.element.classList.toggle('header--menu--open');
+    this.element.classList.toggle('header__menu--open');
 
     // Toggle the dropdown's visibility with a separate class
     this._toggleDropdown();
@@ -35,10 +35,18 @@ export default class extends Controller {
   }
 
   _toggleLogo() {
-    if (this.element.classList.contains('header--menu--open')) {
+    if (this.element.classList.contains('header__menu--open')) {
       this.logoTarget.src = '/assets/logo_black.svg'; // Switch to black logo
     } else {
       this.logoTarget.src = '/assets/logo.svg'; // Switch back to default logo
     }
+  }
+
+  toggleProfileDropdown() {
+    console.log('Profile dropdown clicked!');
+    // Toggle class to show/hide profile dropdown
+    this.profileDropdownTarget.classList.toggle(
+      'header__profile-dropdown--open'
+    );
   }
 }
