2a6f6c5ed1ec5ab17f95547087903e63390ad844
akabishau <aleksey.kabishau@icloud.com>
2024-10-20 21:06:50 -0400

    update layout and add layout styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     # TODO: reorder groups by applying a custom order
+    # TODO: review sending only relevant data to the view
     @books_by_status = current_user.user_books.includes(:book).group_by(&:status)
   end
 end
